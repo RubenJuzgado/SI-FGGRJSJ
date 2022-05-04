0583a08b9daebd323e8a183013171b6ec135de96
Ferno13 <f.gonzalezg.2019@alumnos.urjc.es>
2022-05-04 12:36:07 +0200

    ConAccuracy

--- IA/IA.py
+++ IA/IA.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 import json
 import plotly.graph_objects as go
 
@@ -16,50 +16,77 @@ def regresionLineal():
     usersClasesTest = []
     vulnerableTrain = []
     vulnerableTest = []
-    for i in range(int((len(usersClasesDatos) * 0.6))):
+    vulnerableTestPredecir = []
+    usersPredecirTest = []
+    for i in range(int((len(usersClasesDatos) * 0.69))):
         vulnerableTrain.append(usersClasesDatos[i]['vulnerable'])
 
         usersClasesTrain.append(
             [usersClasesDatos[i]['emails_phishing_recibidos'], usersClasesDatos[i]['emails_phishing_clicados']])
 
-    for i in range(int((len(usersClasesDatos) * 0.6)), len(usersClasesDatos)):
+    for i in range(int((len(usersClasesDatos) * 0.69)), len(usersClasesDatos)):
         vulnerableTest.append(usersClasesDatos[i]['vulnerable'])
         usersClasesTest.append(
             [usersClasesDatos[i]['emails_phishing_recibidos'], usersClasesDatos[i]['emails_phishing_clicados']])
 
-    print("UsersClasesTrain:")
-    print(usersClasesTrain)
-    print("UsersClasesTest:")
-    print(usersClasesTest)
-    print("VulnerableTrain:")
-    print(vulnerableTrain)
-    print("VulnerableTest:")
-    print(vulnerableTest)
-
     regr = linear_model.LinearRegression()
     regr.fit(usersClasesTrain, vulnerableTrain)
-    print("Regr.coef_:")
-    print(regr.coef_)
-    usersPredecir_pred = regr.predict(usersClasesTest)
+    usersClases_pred = regr.predict(usersClasesTest)
     porcentajeClickados = []
-    coefDeter = r2_score(vulnerableTest, usersPredecir_pred)
-    print(coefDeter)
+    coefDeter = r2_score(vulnerableTest, usersClases_pred)
     for i in range(len(usersClasesTest)):
         if usersClasesTest[i][0] != 0:
             data = usersClasesTest[i][1] / usersClasesTest[i][0]
             porcentajeClickados.append(data)
         else:
             porcentajeClickados.append(0)
-
-    print("PorcentajeClickados")
-    print(porcentajeClickados)
-
-    print("regr.intercept")
-    print(regr.intercept_)
-
     plt.scatter(porcentajeClickados, vulnerableTest)
     plt.plot(np.array(porcentajeClickados) * coefDeter + regr.intercept_, porcentajeClickados)
     plt.show()
+
+    for i in range(len(usersClases_pred)):
+        if usersClases_pred[i] >= 0.65:
+            usersClases_pred[i] = 1
+        else:
+            usersClases_pred[i] = 0
+    accuracy = accuracy_score(vulnerableTest, usersClases_pred)
+    # Predicciones
+    with open('static/users_IA_clases.json') as json_file:
+        usersPredecirDatos = json.load(json_file)
+    usersPredecirDatos = usersPredecirDatos['usuarios']
+    usersPredecirTest = []
+    porcentajeClickadosPredecir = []
+    for i in range(int(len(usersClasesDatos))):
+        usersPredecirTest.append(
+            [usersPredecirDatos[i]['emails_phishing_recibidos'], usersPredecirDatos[i]['emails_phishing_clicados']])
+
+    for i in range(len(usersPredecirTest)):
+        if usersPredecirTest[i][0] != 0:
+            data = usersPredecirTest[i][1] / usersPredecirTest[i][0]
+            porcentajeClickadosPredecir.append(data)
+        else:
+            porcentajeClickadosPredecir.append(0)
+    for i in range(len(porcentajeClickadosPredecir)):
+        if porcentajeClickadosPredecir[i] >= 0.65:
+            porcentajeClickadosPredecir[i] = 1
+        else:
+            porcentajeClickadosPredecir[i] = 0
+    usersPredecir_pred = regr.predict(usersPredecirTest)
+    contVuln = 0
+    contNoVuln= 0
+    for i in range(len(usersPredecir_pred)):
+        if usersPredecir_pred[i] >= 0.628:
+            usersPredecir_pred[i] = 1
+            contVuln = 1+contVuln
+
+        else:
+            usersPredecir_pred[i] = 0
+            contNoVuln = 1+contNoVuln
+
+    accuracy2 = accuracy_score(porcentajeClickadosPredecir, usersPredecir_pred)
+    print("Usuarios vulnerables: "+str(contVuln))
+    print("Usuarios NO vulnerables: " + str(contNoVuln))
+    print("El accuracy es de:" + str(accuracy2))
 
 
 if __name__ == '__main__':
